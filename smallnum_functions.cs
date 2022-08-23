67f2132d8f73d11e47e9502ec199cd25a87d8112
sindhu7022 <76683190+sindhu7022@users.noreply.github.com>
2022-08-23 17:52:44 +0530

    Add files via upload

---
+++ smallnum_functions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class SmallNum
+    {
+        public static void Num(int num1, int num2, out int result)
+        {
+            if (num1 < num2)
+            {
+                result = num1;
+            }
+            else
+            {
+                result = num2;
+            }
+        }
+        public static void Main()
+        {
+            int num1, num2;
+            Console.WriteLine("Enter First Number");
+
+
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second Number");
+
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+
+            Num(num1, num2, out int result);
+            Console.WriteLine($"The smaller number is {result}");
+        }
+
+
+
+
+    }
+}
