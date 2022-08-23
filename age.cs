b06ad1525f85407bacf6a9524cdd13ec16b2c1f0
sindhu7022 <76683190+sindhu7022@users.noreply.github.com>
2022-08-23 16:59:42 +0530

    Add files via upload

---
+++ age.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class age
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            
+            age = age + 10;
+	    Console.Write($"after 10 years: {age}");
+            
+        }
+    }
+}
